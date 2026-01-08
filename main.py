44f9f3c3a5b7e49560a20363d6a79c613601c852
Amirmahdi Farzane <102407739+amirmfarzane@users.noreply.github.com>
2026-01-08 18:08:46 +0330

    feat: add book addition functionality

--- main.py
+++ main.py
@@ -12,3 +12,17 @@ def load_books():
 def save_books(books):
     with open(BOOKS_FILE, "w", encoding="utf-8") as f:
         json.dump(books, f, ensure_ascii=False, indent=4)
+
+
+def add_book(books):
+    title = input("Title: ")
+    author = input("Author: ")
+    year = input("Year: ")
+    book = {"id": len(books) + 1, "title": title, "author": author, "year": year}
+    books.append(book)
+    save_books(books)
+    print("Book added!")
+
+if __name__ == "__main__":
+    books = load_books()
+    add_book(books)
